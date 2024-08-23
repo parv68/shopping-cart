15f403a53ca8651099a086e60902838bdb60c795
Parv <parvruhil68@gmail.com>
2024-08-23 21:39:22 +0530

    js file 2

--- script.js
+++ script.js
@@ -100,3 +100,33 @@ products.forEach(
       `;
     }
   );
+
+  class ShoppingCart {
+    constructor() {
+      this.items = [];
+      this.total = 0;
+      this.taxRate = 8.25;
+    }
+  
+    addItem(id, products) {
+      const product = products.find((item) => item.id === id);
+      const { name, price } = product;
+      this.items.push(product);
+  
+      const totalCountPerProduct = {};
+      this.items.forEach((dessert) => {
+        totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+      })
+  
+      const currentProductCount = totalCountPerProduct[product.id];
+      const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+  
+      currentProductCount > 1 
+        ? currentProductCountSpan.textContent = `${currentProductCount}x`
+        : productsContainer.innerHTML += `
+        <div id="dessert${id}" class="product">
+  
+        </div>
+        `;
+    }
+  };
