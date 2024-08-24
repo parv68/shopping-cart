dc53422b36081b65f65db4bc62f9adad0a1f8a7a
Parv <parvruhil68@gmail.com>
2024-08-24 09:57:10 +0530

    final v of js

--- script.js
+++ script.js
@@ -133,7 +133,44 @@ class ShoppingCart {
       `;
   }
 
+  getCounts() {
+    return this.items.length;
+  }
 
+  clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
 };
 
 const cart = new ShoppingCart();
@@ -143,6 +180,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
   (btn) => {
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
     })
   }
 );
@@ -152,3 +191,5 @@ cartBtn.addEventListener("click", () => {
   showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
+
+clearCartBtn.addEventListener('click', cart.clearCart.bind(cart))
